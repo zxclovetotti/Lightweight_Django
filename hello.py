5fb54decf6fc1443146c4dae55e184c9c957adeb
zxclovetotti <zxclovetotti@gmail.com>
2017-07-11 22:05:42 +0800

    update secret_key

--- hello.py
+++ hello.py
@@ -6,11 +6,14 @@ from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
     DEBUG=DEBUG,
     SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES={
         'django.middleware.common.CommonMiddleware',
